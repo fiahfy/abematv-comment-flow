f1801370e4e42a257503ae13282a56524c9a374d
fiahfy <d.fiahfy@gmail.com>
2016-10-11 16:14:35 +0900

    Attach event

--- src/content_scripts.js
+++ src/content_scripts.js
@@ -1,1 +1,20 @@
 let a = 'b'
+
+console.log(a);
+
+setTimeout(() => {
+// const target = document.getElementById('some-id');
+const target = document.querySelector('[class*=styles__comment-list-wrapper___]>div');
+console.log(document);
+console.log(target);
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    console.log(mutation.type);
+  });
+});
+
+const config = { attributes: false, childList: true, characterData: false };
+
+observer.observe(target, config);
+}, 5000)
