0a0a661da41276903df99703a464e7dc25be4f56
fiahfy <d.fiahfy@gmail.com>
2016-10-21 14:44:44 +0900

    Update

--- src/content_scripts.js
+++ src/content_scripts.js
@@ -18,25 +18,60 @@ function hook() {
   // const target = document.getElementById('some-id');
   const target = document.querySelector('[class*=styles__comment-list-wrapper___]>div');
   // console.log(document.body.innerHTML);
-  console.log(target);
+  // console.log(target);
   if (!target) {
     return;
   }
 
-  const containers = document.querySelectorAll('[class*=styles__tv-container___]>[class*=styles__container___]');
-  for (let container of containers) {
-    container.style.width = window.innerWidth;
+function fix() {
+  const container = document.querySelector('[class*=styles__tv-container___]>[class*=styles__resize-screen___]');
+  // const ob = new MutationObserver((mutations) => {
+  //   console.log('fire');
+    // container.style.width = window.innerWidth + 'px!important';
+    // container.style.height = window.innerHeight + 'px!mportant';
+    const width = window.innerWidth + 'px!important';
+    const height = window.innerHeight + 'px!important';
+  // });
+  // const cf = { attributes: true, childList: false, characterData: false };
+  // ob.observe(container, cf);
+  const s = document.styleSheets[0];
+  const ss = `
+  [class*=styles__tv-container___]>[class*=styles__resize-screen___] {
+    width: ${width};
+    height: ${height};
   }
-  console.log(containers);;
+  `
+  s.insertRule(ss, s.cssRules.length);
+}
+  fix();
+  window.addEventListener('resize', fix);
+
+
 
   let i = 0;
+  let lines = [];
   const observer = new MutationObserver((mutations) => {
     const elements = target.querySelectorAll('[class*=styles__animation___]>div>div');
-
+    if (elements.length > 10) {
+      return;
+    }
     // console.log(elements);
     for (let element of elements) {
       const comment = element.querySelector('p').innerHTML;
       // console.log(comment);
+console.log(lines);
+      let index = lines.findIndex((item) => {
+        return !item;
+      });
+      console.log(index);
+      if (index === -1) {
+        lines.push(comment);
+        index = lines.length - 1;
+      } else {
+        lines[index] = comment;
+      }
+      const top = (20 * index) + 'px';
+
 
       const animationName = `translate-${++i}`;
 
@@ -51,11 +86,14 @@ function hook() {
       color: white;
       animation: ${animationName} 5s;
       animation-timing-function: linear;
+      top: ${top};
       `);
 
       div.addEventListener('animationend', () => {
         // console.log('end = ' + comment);
         div.parentNode.removeChild(div);
+        lines[index] = null;
+        // console.log(index);
       });
       document.querySelector('#acf-comment-wrapper').appendChild(div);
 
